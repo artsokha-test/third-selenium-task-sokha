3d7886aabc68fbce29d5ec843a4ed166b7acd8ba
artsokha-test <artur.sokha@rolique.io>
2018-06-04 23:02:49 +0300

    base

---
+++ src/main/java/common/WebDriverFactory.java
@@ -0,0 +1,12 @@
+package common;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverFactory {
+
+    private WebDriver driver;
+
+    public WebDriverFactory(BrowserEnum, browserEnum){
+        if(browserEnum)
+    }
+}
