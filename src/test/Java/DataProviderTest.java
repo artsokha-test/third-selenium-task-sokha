1b51ae1662c60e43e9dcd156aa50d1fef6839e2d
artsokha-test <artur.sokha@rolique.io>
2018-06-06 16:21:49 +0300

    fixed data provider

--- src/test/Java/DataProviderTest.java
+++ src/test/Java/DataProviderTest.java
@@ -1,11 +1,13 @@
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.DataProvider;
 
 public class DataProviderTest {
 
     @DataProvider(name="Authentication")
-    public static Object[][] Authentication() {
-        return new Object[][] {{"testuser_1", "Test123"}, {"testuser_2", "test123"}};
+    public static Object[][] sdfsdf() {
+        return new Object[][] {
+                {"TestFirstName_1", "TestLastName_1", "222-333-444", "TestUsername_1", "test1@mail.com","This is test user 1", "test123"},
+                {"TestFirstName_2", "TestLastName_2", "222-333-444", "TestUsername_2", "test2@mail.com","This is test user 1", "test123"},
+                };
     }
 
 }
--- src/test/Java/RegistrationProcessTest.java
+++ src/test/Java/RegistrationProcessTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class RegistrationProcessTest extends TestBase{
 
 
-    @Test(dataProvider="Authentication")
+    @Test(dataProvider="Authentication", dataProviderClass = DataProviderTest.class)
     public void homePageTest(String sName, String sLastname){
         homePage.clickRegistrationButton()
                 .fillNameInput(sName)
